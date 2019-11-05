ef7dda567dff73a2711d419f9526e12bd4ad2aa8
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-11-05 09:26:59 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan45-Cetak Nama/src/if01/pkg10118016/latihan45/cetak/nama/IF0110118016Latihan45CetakNama.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118016.latihan45.cetak.nama;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rezky
+ * NAMA      : Rezky Nur Fauzi
+ * KELAS     : IF-1
+ * NIM       : 10118016
+ * Deskripsi Program   : Program ini berisi program Cetak Nama
+ */
+
+public class IF0110118016Latihan45CetakNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+        
+        Printer objP = new Printer();
+        
+        System.out.println("=====Aplikasi Pencetak Nama=====");
+        System.out.print("Masukan Nama Anda : ");
+        objP.setNama(scan1.nextLine());
+        System.out.print("Mau Cetak Berapa Kali? : ");
+        objP.setJmlCetak(scan2.nextInt());
+        objP.cetak(objP.getNama());
+        System.out.println("Hasil Cetak : ");
+        objP.cetak(objP.getJmlCetak(), objP.getNama());
+    }
+    
+}
